484df2162c0d114b8ec1960caa450489173bb005
kvjp2209 <nguyenthekien20011@gmail.com>
2025-09-17 17:49:34 +0700

    icon size , google maps foooter

--- src/components/home/footer.tsx
+++ src/components/home/footer.tsx
@@ -6,8 +6,20 @@ const Footer = () => {
         <div>
           <h3 className="text-lg font-semibold mb-3">Địa chỉ</h3>
           <p className="text-gray-300">
-            SN 15-17 Ông Ích Kiêm , Phường Cao Xanh
+            SN 15-17 Ông Ích Khiêm , Phường Cao Xanh
           </p>
+          <div className="mt-4">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3725.638527899829!2d107.08032567586659!3d20.967026580666968!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a590e51765ad3%3A0x21dd1e160cadf04!2zU24gMTUtMTcgw7RuZyDDrWNoIGtoacOqbQ!5e0!3m2!1svi!2s!4v1758102237623!5m2!1svi!2s"
+              width="100%"
+              height="200"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              className="rounded-md"
+            />
+          </div>
         </div>
 
         {/* Hotline */}
